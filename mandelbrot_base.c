725342620de7c73e71246b0411915cfa5b70f813
brncluis <fockluis@gmail.com>
2026-08-29 16:21:03 -0300

    alteração de esspaço a mais no final de toda linha, sem diferencas dos testes agr

--- mandelbrot_base.c
+++ mandelbrot_base.c
@@ -25,11 +25,11 @@ int mandelbrot_ponto(double c_real, double c_imag, int max_iteracao) {
 
 void calcular_cor(unsigned char *imagem, int linha, int largura, int altura, int max_iteracao) {
 
-    double c_imag = -1.5 + ((double) linha / (altura - 1)) * 3.0;
+    double c_imag = -1.5 + ((double) linha / altura) * 3.0;
 
     for (int coluna = 0; coluna < largura; coluna++) {
 
-        double c_real = -2.0 + ((double) coluna / (largura - 1)) * 3.0;
+        double c_real = -2.0 + ((double) coluna / largura) * 3.0;
 
         int iteracoes = mandelbrot_ponto(c_real, c_imag, max_iteracao);
         unsigned char intensidade = (unsigned char) (255.0 * iteracoes / max_iteracao);
@@ -52,7 +52,13 @@ int gera_pgm(unsigned char *imagem, int largura, int altura, char *nome) {
     for (int linha = 0; linha < altura; linha++) {
         for (int coluna = 0; coluna < largura; coluna++) {
             int posicao_lista = linha * largura + coluna;
-            fprintf(arquivo, "%d ", imagem[posicao_lista]);
+
+            fprintf(arquivo, "%d", imagem[posicao_lista]);
+
+            if (coluna < largura - 1) {
+                fprintf(arquivo, " ");
+            }
+
         }
         fprintf(arquivo, "\n");
     }
