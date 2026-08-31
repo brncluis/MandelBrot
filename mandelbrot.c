5617b9373f0b4a4712e47dfc3c0aea74c5e23d1f
brncluis <fockluis@gmail.com>
2026-08-31 18:06:24 -0300

    main para todos os modos

---
+++ mandelbrot.c
@@ -0,0 +1,40 @@
+#include "mandelbrot_base.h"
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 5) {
+        reportar_erro("Erro insira mandelbrot largura altura iteracoes threads");
+        return 1;
+    }
+
+    int largura, altura, max_iteracao, qtd_threads;
+
+    if (parse_inteiro_positivo(argv[1], &largura) != 0 ||
+        parse_inteiro_positivo(argv[2], &altura) != 0 ||
+        parse_inteiro_positivo(argv[3], &max_iteracao) != 0 ||
+        parse_inteiro_positivo(argv[4], &qtd_threads) != 0) {
+
+        reportar_erro("Passe apenas numeros inteiros positivos validos");
+        return 1;
+
+    }
+
+    if (rodar_serial(largura, altura, max_iteracao, qtd_threads) != 0) {
+        return 1;
+    }
+
+    if (rodar_openmp(largura, altura, max_iteracao, qtd_threads) != 0) {
+        return 1;
+    }
+
+    if (rodar_pthreads1(largura, altura, max_iteracao, qtd_threads) != 0) {
+        return 1;
+    }
+
+    if (rodar_pthreads2(largura, altura, max_iteracao, qtd_threads) != 0) {
+        return 1;
+    }
+
+    return 0;
+
+}
